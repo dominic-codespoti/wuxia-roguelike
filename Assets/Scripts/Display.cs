da32041c80873d46a10b26b3e549f902645e6637
Dom <domcodespoti@gmail.com>
2024-04-21 22:52:32 +1000

    First level

--- Assets/Scripts/Display.cs
+++ Assets/Scripts/Display.cs
@@ -9,7 +9,7 @@ class Display : MonoBehaviour
 
   public void OnGUI()
   {
-    // Display the player's movement state.
     GUI.Label(new Rect(Screen.width - 50, 0, 100, 20), playerController.movementState.ToString());
+    GUI.Label(new Rect(Screen.width - 50, 20, 100, 20), playerController.direction.ToString());
   }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,7 +13,9 @@ class PlayerController : MonoBehaviour
   public float dashForce = 7.0f;
 
   public MovementState movementState;
+  public Direction direction;
 
+  private Animator animator;
   private Rigidbody2D body;
   private SpriteRenderer spriteRenderer;
   private new BoxCollider2D collider;
@@ -27,6 +29,10 @@ class PlayerController : MonoBehaviour
     spriteRenderer.sortingOrder = 1;
 
     collider = GetComponent<BoxCollider2D>();
+    animator = GetComponent<Animator>();
+
+    direction = Direction.Down;
+    movementState = MovementState.Idle;
   }
 
   public void Update()
@@ -35,6 +41,7 @@ class PlayerController : MonoBehaviour
     var vertical = Input.GetAxis("Vertical");
     var isDashing = Input.GetButtonDown("Jump");
 
+    direction = GetDirection(horizontal, vertical);
     movementState = (horizontal, vertical, isDashing) switch
     {
       (0, 0, false) => MovementState.Idle,
@@ -46,9 +53,11 @@ class PlayerController : MonoBehaviour
     {
       case MovementState.Idle:
         body.velocity = Vector2.zero;
+        SetAnimatorParameters("Idle");
         break;
       case MovementState.Walking:
         body.velocity = new Vector2(horizontal, vertical) * speed;
+        SetAnimatorParameters("Walking", direction);
         break;
       case MovementState.Dashing:
         body.AddForce(new Vector2(horizontal, vertical) * dashForce, ForceMode2D.Impulse);
@@ -56,12 +65,33 @@ class PlayerController : MonoBehaviour
     }
   }
 
-  public void OnCollisionEnter2D(Collision2D collision)
+  private Direction GetDirection(float horizontal, float vertical)
   {
-    Debug.Log($"Collision with {collision.gameObject.name}");
-    if (collision.gameObject.CompareTag("Wall"))
+    if (Mathf.Abs(horizontal) > Mathf.Abs(vertical))
     {
-      body.velocity = Vector2.zero;
+      return horizontal > 0 ? Direction.Right : Direction.Left;
+    }
+    else
+    {
+      return vertical > 0 ? Direction.Up : Direction.Down;
+    }
+  }
+
+  private void SetAnimatorParameters(string state, Direction? direction = null)
+  {
+    animator.SetBool("IsIdle", state == "Idle");
+    animator.SetBool("IsWalkingLeft", direction == Direction.Left && state == "Walking");
+    animator.SetBool("IsWalkingRight", direction == Direction.Right && state == "Walking");
+    animator.SetBool("IsWalkingUp", direction == Direction.Up && state == "Walking");
+    animator.SetBool("IsWalkingDown", direction == Direction.Down && state == "Walking");
+
+    if (direction == Direction.Left)
+    {
+      spriteRenderer.flipX = true;
+    }
+    else if (direction == Direction.Right)
+    {
+      spriteRenderer.flipX = false;
     }
   }
 }
@@ -73,3 +103,10 @@ enum MovementState
   Dashing
 }
 
+enum Direction
+{
+  Up,
+  Down,
+  Left,
+  Right
+}
