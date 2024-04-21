229e676952011069c8747e1b0ef22ece352f608a
Dom <domcodespoti@gmail.com>
2024-04-21 15:22:27 +1000

    Use defined world

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 /// <summary>
 /// A 2D player controller.
 /// </summary>
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(BoxCollider2D))]
 class PlayerController : MonoBehaviour
 {
   [Range(0.0f, 10.0f)]
@@ -14,16 +14,23 @@ class PlayerController : MonoBehaviour
 
   public MovementState movementState;
 
+  private Rigidbody2D body;
+  private SpriteRenderer spriteRenderer;
+  private new BoxCollider2D collider;
+
   public void Start()
   {
-    var body = GetComponent<Rigidbody2D>();
+    body = GetComponent<Rigidbody2D>();
     body.gravityScale = 0;
+
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    spriteRenderer.sortingOrder = 1;
+
+    collider = GetComponent<BoxCollider2D>();
   }
 
   public void Update()
   {
-    var body = GetComponent<Rigidbody2D>();
-
     var horizontal = Input.GetAxis("Horizontal");
     var vertical = Input.GetAxis("Vertical");
     var isDashing = Input.GetButtonDown("Jump");
@@ -39,13 +46,22 @@ class PlayerController : MonoBehaviour
     {
       case MovementState.Idle:
         body.velocity = Vector2.zero;
-      break;
+        break;
       case MovementState.Walking:
         body.velocity = new Vector2(horizontal, vertical) * speed;
-      break;
+        break;
       case MovementState.Dashing:
         body.AddForce(new Vector2(horizontal, vertical) * dashForce, ForceMode2D.Impulse);
-      break;
+        break;
+    }
+  }
+
+  public void OnCollisionEnter2D(Collision2D collision)
+  {
+    Debug.Log($"Collision with {collision.gameObject.name}");
+    if (collision.gameObject.CompareTag("Wall"))
+    {
+      body.velocity = Vector2.zero;
     }
   }
 }
